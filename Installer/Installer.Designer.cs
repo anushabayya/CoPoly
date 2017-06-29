a1e47ef958cdd374d6219e07460d9bf3a015f78e
tamil2915 <tamilmaran001@gmail.com>
2017-06-30 03:21:26 +0530

    Implemented installer application for the multi paste application. Removed the reg file as I have modified the registry through code. Installer class takes care of installing the application and adding the registry key. whenever multipaste.exe is modified, copy it into the "exe" folder inside "Installer" project and reinstall the application. Currently the previously installed location will possess the copy of older multipaste.exe file. Will add support to remove that in upcoming commits.

---
+++ Installer/Installer.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Installer
+{
+    partial class Installer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FOLDER_BASE_PATH_TEXT_BOX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BROWSE_BUTTON = new System.Windows.Forms.Button();
+            this.INSTALL = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FOLDER_BASE_PATH_TEXT_BOX
+            // 
+            this.FOLDER_BASE_PATH_TEXT_BOX.Location = new System.Drawing.Point(76, 23);
+            this.FOLDER_BASE_PATH_TEXT_BOX.Name = "FOLDER_BASE_PATH_TEXT_BOX";
+            this.FOLDER_BASE_PATH_TEXT_BOX.Size = new System.Drawing.Size(200, 20);
+            this.FOLDER_BASE_PATH_TEXT_BOX.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Folder path";
+            // 
+            // BROWSE_BUTTON
+            // 
+            this.BROWSE_BUTTON.Location = new System.Drawing.Point(284, 22);
+            this.BROWSE_BUTTON.Name = "BROWSE_BUTTON";
+            this.BROWSE_BUTTON.Size = new System.Drawing.Size(29, 20);
+            this.BROWSE_BUTTON.TabIndex = 2;
+            this.BROWSE_BUTTON.Text = "...";
+            this.BROWSE_BUTTON.UseVisualStyleBackColor = true;
+            this.BROWSE_BUTTON.Click += new System.EventHandler(this.FileBrowse);
+            // 
+            // INSTALL
+            // 
+            this.INSTALL.Location = new System.Drawing.Point(222, 49);
+            this.INSTALL.Name = "INSTALL";
+            this.INSTALL.Size = new System.Drawing.Size(92, 27);
+            this.INSTALL.TabIndex = 3;
+            this.INSTALL.Text = "Install";
+            this.INSTALL.UseVisualStyleBackColor = true;
+            this.INSTALL.Click += new System.EventHandler(this.InstallMultiPaste);
+
+            // 
+            // Installer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 88);
+            this.Controls.Add(this.INSTALL);
+            this.Controls.Add(this.BROWSE_BUTTON);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FOLDER_BASE_PATH_TEXT_BOX);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Installer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MultiPaste installer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BROWSE_BUTTON;
+        private System.Windows.Forms.Button INSTALL;
+        private System.Windows.Forms.TextBox FOLDER_BASE_PATH_TEXT_BOX;
+    }
+}
+
---
+++ Installer/Installer.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Installer
+{
+    public partial class Installer : Form
+    {
+        public Installer()
+        {
+            InitializeComponent();
+        }
+
+        private void FileBrowse(object sender, EventArgs e)
+        {
+            //here open the file browse dialog for user
+            string folderName;
+
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Here update the text control with the obtained folder path
+                folderName = folderDialog.SelectedPath;
+
+                //show it to the user in the text box
+                this.FOLDER_BASE_PATH_TEXT_BOX.Text = folderName;
+            }
+        }
+
+        private void InstallMultiPaste(object sender, EventArgs e)
+        {
+            //get the folder path here
+            string folderPath = this.FOLDER_BASE_PATH_TEXT_BOX.Text;
+
+            String formattedPath = FormatFolderPath(folderPath);
+
+            //check for the folder availability here
+            bool folderExists = Directory.Exists(folderPath);
+
+            formattedPath = Path.Combine(formattedPath, "MultiplePaste.exe");
+
+            if (!folderExists)
+            {
+                MessageBox.Show("Folder doesn't exist", "Folder error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //The folder is valid
+            //Thus copy our multi paste exe to the specified folder
+            bool exeCopied = CopyExeToFolder();
+            if (exeCopied)
+            {
+                //add the folder entry to the registry
+                RegistryKey myKey = Registry.ClassesRoot.CreateSubKey("Directory\\Background\\shell\\Multiple Paste\\command");
+
+                try
+                {
+                    if (myKey != null)
+                    {
+                        myKey.SetValue("", formattedPath);
+                        myKey.Close();
+                    }
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+                MessageBox.Show("Installed successfully!", "Installation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+            else
+                MessageBox.Show("There was an error during installation.", "Installation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+                
+        private bool CopyExeToFolder()
+        {
+            bool retVal = false;
+
+            //try to copy the file here
+            try
+            {
+                String sourceFile = "./../../exe/MultiplePaste.exe";
+
+                string destinationFile = this.FOLDER_BASE_PATH_TEXT_BOX.Text;
+                string fileName = "MultiplePaste.exe";
+
+                destinationFile = Path.Combine(destinationFile, fileName);
+                string sourceFullPath = Path.GetFullPath(sourceFile);
+                File.Copy(sourceFile, destinationFile, true);
+                retVal = true;
+            }
+            catch(UnauthorizedAccessException)
+            {
+                MessageBox.Show("You don't have required permissions to the installation folder.", "Copy error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (PathTooLongException)
+            {
+                MessageBox.Show("The installation folder path is too long. Make sure it doesn't exceed 260 characters.", "Copy error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("The installation directory doesn't exist. ", "Copy error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (NotSupportedException)
+            {
+                MessageBox.Show("Fatal error. contact our support team.", "Folder error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return retVal;
+        }
+        private String FormatFolderPath(string folderPath)
+        {
+            //we need to split the name with '\' as delimiter
+            
+            String[] folderPathStringArray = folderPath.Split('\\');
+            String retString = "";
+
+            //loop through the array and append double slashes in between
+            for (int i = 0; i < folderPathStringArray.Length; i++)
+            {
+                if (folderPathStringArray[i].Length == 0)
+                {
+                    retString += "\\";
+                    continue;
+                }
+                retString += folderPathStringArray[i];
+                if (i != folderPathStringArray.Length - 1)
+                    retString += "\\\\";
+            }
+
+            return retString;
+        }
+    }
+}
